972064e653c52d522f968256d0a67084329cf2e7
Lee Ji Eon <ez_on@naver.com>
2025-07-19 17:19:50 +0900

    Complete Initial Setting

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    // Tailwind Test
+    <div className="min-h-screen flex items-center justify-center bg-green-500 text-white">
+      <h1 className="text-3xl font-bold">
+        Tailwind is Working!
+      </h1>
+    </div>
+  );
+
+}
+
+export default App;
